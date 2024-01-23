6d948281ccd4927a8c3663956fddd0ee390f4523
argo <shuohao.huang.7e7@itb.cat>
2024-01-23 13:45:55 +0100

    closes #2

---
+++ Msg/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Msg
+{
+    public class Class1
+    {
+
+    }
+}
--- RemakeHvsM/Game.cs
+++ RemakeHvsM/Game.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Constants;
 using Stats;
+using Checker;
+using Menssages;
 
 namespace Game
 {
@@ -8,6 +10,8 @@ namespace Game
     {
         public static void Main()
         {
+            int roundCounter= 0,
+            remainingAttempts = GameConstant.MaxAttempts;
             int[] heroes = { 0, 1, 2, 3 },
                 remainHeroes = heroes,
                 abilityEffect = { 2, 100, 3, 500 },
@@ -17,10 +21,50 @@ namespace Game
                                  { { 200, 150, 300, 70,300 } , { 300, 250, 400, 120,400 }  },
                                  { { 25, 35, 20, 25,20 } , { 35, 45, 35, 40,30 } }
             };
+
             float[,] maxValues = new float[5, 3],
                 currentValues = new float[5, 3];
 
-            string[] name = { Constant.ArcherName, Constant.BarbarianName, Constant.MageName, Constant.DruidName, Constant.MonsterName };
+            bool validInput,
+                hasRemainingAttemptsMenu = true,
+                hasRemainingAttempts = true;
+            bool[] isHero = { true, true, true, true, false };
+
+            string userCommand; 
+
+            string[] name = { Constant.ArcherName, Constant.BarbarianName, Constant.MageName, Constant.DruidName, Constant.MonsterName },
+                twoValidInputs = [Constant.OneStr, Constant.ZeroStr],
+                threeValidInputs =
+                    [Constant.OneStr, Constant.TwoStr, Constant.ThreeStr],
+                fourValidInputs =
+                    [
+                        Constant.OneStr,
+                        Constant.TwoStr,
+                        Constant.ThreeStr,
+                        Constant.FourStr
+                    ],
+                StatsRequirementMsg =
+
+                    [
+                        Constant.HpMenuMsg + Constant.RangedInMsg,
+                        Constant.AttackMenuMsg + Constant.RangedInMsg,
+                        Constant.DmgReductionMenuMsg + Constant.RangedInMsg
+                    ];
+            
+
+            do
+            {
+                userCommand = Console.ReadLine() ?? "";
+                validInput = Check.ValidateInput(userCommand, twoValidInputs);
+                Msg.ValidateInput(
+                    ref remainingAttempts,
+                    ref hasRemainingAttemptsMenu,
+                    validInput,
+                    Constant.ErrorEndMsg
+                );
+            } while (!validInput && hasRemainingAttemptsMenu);
+
+
             bool[] Alive = { true, true, true, true, true };
             for (int i = 0; i < maxValues.GetLength(0); i++)
             {
@@ -28,9 +72,6 @@ namespace Game
 
             }
             currentValues = maxValues;
-
-            
-
         }
     }
 }
